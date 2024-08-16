ba31e906e7fb043587174509e597dda2cde3971f
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-08-16 23:21:25 +0700

    feat: add cors

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const cors = require('cors')
+
+app.uss(cors())
+
 // Import Body Parser
 const bodyParser = require('body-parser');
 
